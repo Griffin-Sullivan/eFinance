7bef09719db3989879560ba75e3aca9bc52ab2b3
Dylan Tellei <dylantellei@gmail.com>
2021-04-11 16:18:21 -0400

    Updated LoanApplication Code

--- IST412-eFinance/src/controllers/LoanController.java
+++ IST412-eFinance/src/controllers/LoanController.java
@@ -6,29 +6,78 @@
 package controllers;
 
 import eFinance.Main;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import models.LoanApplication;
 
 /**
  *
  * @author Jared
  */
-public class LoanController implements Initializable{
+public class LoanController implements Initializable {
+
     private Main application;
-    
-  
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField income;
+
+    @FXML
+    private TextField ssn;
+
+    @FXML
+    private TextField loanAmnt;
+
+    @FXML
+    private Text fNameLabel;
+
+    @FXML
+    private Text lNameLabel;
+
+    @FXML
+    private Text incomeLabel;
+
+    @FXML
+    private Text loanAmntLabel;
+
+    @FXML
+    private Button applyBtn;
+
+    @FXML
+    private Text ssnLabel;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-    
+
     }
-      
-        public void setApp(Main application){
+
+    public void setApp(Main application) {
         this.application = application;
     }
+    
+    @FXML
+     void apply(ActionEvent event) throws IOException{
+        // this is when the user hits the apply button
+        String fName = firstName.getText();
+        String lName = lastName.getText();
+        double incomeA = Double.parseDouble(income.getText());
+        String ssnA = ssn.getText();
+        double loanAmntA = Double.parseDouble(loanAmnt.getText());
         
-    public void apply()
-    {
-    // this is when the user hits the apply button
+        LoanApplication newLoan = new LoanApplication(fName,lName,incomeA,ssnA,loanAmntA);
+        
+        System.out.println(newLoan.getAll());
     }
 }
--- IST412-eFinance/src/models/LoanApplication.java
+++ IST412-eFinance/src/models/LoanApplication.java
@@ -8,24 +8,26 @@ package models;
 public class LoanApplication {
     private final String firstName;
     private final String lastName;
-    private final int age;
+    private int age;
     private final double income;
     private final String ssn;
     private final double desiredLoanAmount;
-    private final String id;
+    private String id;
 
-    public LoanApplication(String firstName, String lastName, int age, double income, String ssn, double desiredLoanAmount, String id) {
+    public LoanApplication(String firstName, String lastName, double income, String ssn, double desiredLoanAmount) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.age = age;
         this.income = income;
         this.ssn = ssn;
         this.desiredLoanAmount = desiredLoanAmount;
-        this.id = id;
     }
     
     public String getApplicationId() {
         return this.id;
+    }
+    
+    public String getAll(){
+        return "First Name: " + this.firstName + "\nLast Name:" + this.lastName + "\nIncome: $" + this.income + "\nSSN: " + this.ssn + "\nLoan Amount: $" + this.desiredLoanAmount;
     }
     
     
